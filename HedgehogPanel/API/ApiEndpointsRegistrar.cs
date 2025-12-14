3189503f16055f90c8f1c05f76115e862f4b4e6a
Batacek <74470527+Batacek@users.noreply.github.com>
2025-12-14 10:50:35 +0100

    Add `AdminEndpoints` mapping to `ApiEndpointsRegistrar`
    - Updated `ApiEndpointsRegistrar` to include `MapAdminEndpoints` for admin functionalities.
    - Enhanced logging to reflect the addition of `Admin` endpoints.

--- HedgehogPanel/API/ApiEndpointsRegistrar.cs
+++ HedgehogPanel/API/ApiEndpointsRegistrar.cs
@@ -1,5 +1,6 @@
 using HedgehogPanel.API.Auth;
 using HedgehogPanel.API.Servers;
+using HedgehogPanel.API.Admin;
 using Serilog;
 
 namespace HedgehogPanel.API;
@@ -9,9 +10,10 @@ public static class ApiEndpointsRegistrar
     private static readonly Serilog.ILogger Logger = Log.ForContext(typeof(ApiEndpointsRegistrar));
     public static IEndpointRouteBuilder MapApi(this IEndpointRouteBuilder endpoints)
     {
-        Logger.Information("Mapping API endpoints: Auth and Servers...");
+        Logger.Information("Mapping API endpoints: Auth, Servers and Admin...");
         endpoints.MapAuthEndpoints();
         endpoints.MapServerEndpoints();
+        endpoints.MapAdminEndpoints();
         Logger.Information("API endpoints mapped.");
         return endpoints;
     }
