c312689828c580e0ba36ada021de6157e5f41f50
Batacek <74470527+Batacek@users.noreply.github.com>
2026-02-07 11:34:37 +0100

    Replace `Serilog` with `HedgehogLogger` in `ApiEndpointsRegistrar` for consistent logging

--- HedgehogPanel/API/ApiEndpointsRegistrar.cs
+++ HedgehogPanel/API/ApiEndpointsRegistrar.cs
@@ -1,13 +1,13 @@
 using HedgehogPanel.API.Auth;
 using HedgehogPanel.API.Servers;
 using HedgehogPanel.API.Admin;
-using Serilog;
+using HedgehogPanel.Core.Logging;
 
 namespace HedgehogPanel.API;
 
 public static class ApiEndpointsRegistrar
 {
-    private static readonly Serilog.ILogger Logger = Log.ForContext(typeof(ApiEndpointsRegistrar));
+    private static readonly ILoggerService Logger = HedgehogLogger.ForContext(typeof(ApiEndpointsRegistrar));
     public static IEndpointRouteBuilder MapApi(this IEndpointRouteBuilder endpoints)
     {
         Logger.Information("Mapping API endpoints: Auth, Servers and Admin...");
