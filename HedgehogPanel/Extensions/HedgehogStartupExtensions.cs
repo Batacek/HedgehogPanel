482f3dcb7e0a4539df40a0686bad93807906c37f
Batacek <74470527+Batacek@users.noreply.github.com>
2026-03-14 13:18:39 +0100

    Add missing `using` directives in `HedgehogStartupExtensions`

--- HedgehogPanel/Extensions/HedgehogStartupExtensions.cs
+++ HedgehogPanel/Extensions/HedgehogStartupExtensions.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
@@ -17,6 +20,11 @@ using Microsoft.AspNetCore.Diagnostics;
 using DotNetEnv;
 using Microsoft.AspNetCore.RateLimiting;
 using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Npgsql;
 
 namespace HedgehogPanel.Extensions;
